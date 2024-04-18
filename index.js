36a315d751d1036a22af79293b48b39d291bae41
anjalydas <164157759+anjalydas@users.noreply.github.com>
2024-04-18 13:07:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let mark = 94
+
+if (mark >= 90)
+{
+    console.log ("A+")
+}
+else if (mark >= 80)
+{
+    console.log ("A")
+}
+else if (mark >= 70)
+{
+    console.log ("B+")
+}
+else if (mark >= 60)
+{
+    console.log ("B")
+}
+else if (mark >= 50)
+{
+    console.log ("C+")
+}
+else 
+{
+    console.log ("Failed")
+}
